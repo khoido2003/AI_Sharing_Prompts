b16f810245ea2e8900bcd01d458dc0c3a566a1d1
Khoi Do <khoizpro31@gmail.com>
2024-01-30 17:29:33 +0700

    Enable access to React Query dev tool in production

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import React from "react";
 import "./globals.css";
 import BackGround from "./ui/BackGround";
 import Nav from "./ui/Nav";
@@ -9,12 +10,28 @@ import { ThemeProvider } from "./utils/theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import("@tanstack/react-query-devtools/build/modern/production.js").then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    }),
+  ),
+);
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const queryClient = new QueryClient();
+
+  const [showDevtools, setShowDevtools] = React.useState(false);
+
+  React.useEffect(() => {
+    // @ts-ignore
+    window.toggleDevtools = () => setShowDevtools((old) => !old);
+  }, []);
+
   return (
     <html lang="en">
       <link rel="icon" href="/assets/images/logo.svg" sizes="any" />
@@ -37,6 +54,12 @@ export default function RootLayout({
           </ThemeProvider>
 
           <ReactQueryDevtools initialIsOpen={false} />
+
+          {showDevtools && (
+            <React.Suspense fallback={null}>
+              <ReactQueryDevtoolsProduction />
+            </React.Suspense>
+          )}
         </QueryClientProvider>
       </body>
     </html>
