4812ac8897e2b85e9d9bddb47f848666f83d5e12
Khoi Do <khoizpro31@gmail.com>
2024-01-30 18:08:34 +0700

    Reverse to use old version

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -1,16 +1,15 @@
 import { connectToDB } from "../../utils/database";
 import Prompt from "../../models/prompt";
 
-export const GET = async (req, res) => {
+export const GET = async (req: Request, res: Response) => {
   try {
     await connectToDB();
     const prompts = await Prompt.find({})
       .populate("creator")
       .sort("-dateAdded");
 
-    res.status(200).json(prompts);
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (err) {
-    console.error("Failed to fetch prompts:", err);
-    res.status(500).send("Failed to fetch prompts!");
+    return new Response("Failed to fetch prompts!", { status: 500 });
   }
 };
